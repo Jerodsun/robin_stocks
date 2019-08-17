a7b4ebe80fa83ceef01f374b18b3353624f9d6ff
Jerodsun <Jerod.sun@live.com>
2019-08-16 18:33:51 -0700

    change setup.py local install

--- setup.py
+++ setup.py
@@ -8,17 +8,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='robin_stocks',
+setup(name='robin_stocks_sun',
       version='0.9.8.8',
       description='A Python wrapper around the Robinhood API',
       long_description=long_description,
-      url='https://github.com/jmfernandes/robin_stocks',
+      url='https://github.com/jerodsun/robin_stocks',
       author='Josh Fernandes',
       author_email='joshfernandes@mac.com',
       keywords=['robinhood','robin stocks','finance app','stocks','options','trading','investing'],
       license='MIT',
       python_requires='>=3',
-      packages=['robin_stocks'],
+      packages=['robin_stocks_sun'],
       requires=['requests'],
       install_requires=[
           'requests',
